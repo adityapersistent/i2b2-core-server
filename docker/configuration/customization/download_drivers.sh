ae96a21a185cacf4ae024bccd376e5f056cc77dc
Aditya vidap <aditya_vidap@persistent.com>
2026-05-25 11:52:24 +0530

    driver versions

--- docker/configuration/customization/download_drivers.sh
+++ docker/configuration/customization/download_drivers.sh
@@ -1,14 +1,16 @@
 
 cd customization/
+
+export jdbc_mssql_driver_version=${jdbc_mssql_driver_version:-8.2.2}
+export jdbc_pg_driver_version=${jdbc_pg_driver_version:-42.7.8}
+
 echo "Downloading MSSQL - $jdbc_mssql_driver_version"
 echo "Downloading PGSQL - $jdbc_pg_driver_version"
 echo "The Oracle driver requires login, so it has already been downloaded."
 
-export jdbc_mssql_driver_version=${jdbc_mssql_driver_version:-8.2.2}
 export jdbc_mssql_driver_download_url=https://github.com/Microsoft/mssql-jdbc/releases/download/v${jdbc_mssql_driver_version}/mssql-jdbc-${jdbc_mssql_driver_version}.jre8.jar
 curl -SOLs ${jdbc_mssql_driver_download_url}
 
-export jdbc_pg_driver_version=${jdbc_pg_driver_version:-42.7.8}
 export jdbc_pg_driver_download_url=https://jdbc.postgresql.org/download/postgresql-${jdbc_pg_driver_version}.jar
 curl -SOLs ${jdbc_pg_driver_download_url}
 
